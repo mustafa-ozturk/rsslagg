2fc66a0916bec8e5ea2f20107a5b3dc5d26d2be9
Mustafa Ozturk <mozturkpro@gmail.com>
2024-12-30 17:42:21 -0500

    refactoring

--- main.go
+++ main.go
@@ -10,6 +10,20 @@ import (
 	"github.com/mustafa-ozturk/rsslagg/internal/config"
 )
 
+func extractTimeFromPubDate(originalPubDate string) (time.Time, error) {
+	splitDate := strings.Split(originalPubDate, " ")
+	joinedDate := strings.Join(splitDate[:len(splitDate) - 2], " ")
+	pubDate, err := time.Parse("Mon, 02 Jan 2006", joinedDate)
+	return pubDate, err
+}
+
+type RSSItemWithChannelTitle struct {
+	ChannelTitle	string	
+	ItemTitle		string	
+	Link			string
+	PubDate			time.Time
+}
+
 func main() {
 	// 1. get feeds from config file
 	cfg, err := config.Read()
@@ -22,7 +36,7 @@ func main() {
 	fmt.Println("config:rss_feed_links[1]:", cfg.RSSFeedLinks[1])
 	fmt.Println("config:rss_feed_links[2]:", cfg.RSSFeedLinks[2])
 
-	rssFeeds := []RSSFeed{}
+	rssItems := []RSSItemWithChannelTitle{}
 	
 	for _, link := range cfg.RSSFeedLinks {
 		feed, err := fetchFeed(context.Background(), link)
@@ -31,34 +45,36 @@ func main() {
 			return
 		}
 
-		rssFeeds = append(rssFeeds, *feed)
-	}
-
-
-	for _, feed := range rssFeeds {
 		for _, item := range feed.Channel.Item {
-			splitDate := strings.Split(item.PubDate, " ")
-			joinedDate := strings.Join(splitDate[:len(splitDate) - 2], " ")
-			pubDate, err := time.Parse("Mon, 02 Jan 2006", joinedDate)
+			pubDate, err := extractTimeFromPubDate(item.PubDate)
 			if err != nil {
-				log.Fatalf("couldn't parse date: %v", err)
+				log.Fatalf("$$ couldn't parse date: %v", err)
 				return
 			}
 
-			pubDateStr := fmt.Sprintf("%04d-%02d-%02d",
-				pubDate.Year(),
-				int(pubDate.Month()),
-				pubDate.Day())
-
-			fmt.Printf("- %s | %s | %s:\n\t%s\n\n",
-				pubDateStr,
-				feed.Channel.Title,
-				item.Title,
-				item.Link) 
+			rssItems = append(rssItems, RSSItemWithChannelTitle{
+				ChannelTitle: feed.Channel.Title,
+				ItemTitle: item.Title,
+				Link:	item.Link,
+				PubDate: pubDate,
+			})
 		}
 	}
 
 
-	// 3. sort by latest
+	for _, item := range rssItems {
+		pubDateStr := fmt.Sprintf("%04d-%02d-%02d",
+			item.PubDate.Year(),
+			int(item.PubDate.Month()),
+			item.PubDate.Day())
+
+		fmt.Printf("- %s | %s | %s:\n\t%s\n\n",
+			pubDateStr,
+			item.ChannelTitle,
+			item.ItemTitle,
+			item.Link) 
+	}
+
+
 	// 4. display latest max posts, default to 10 and ovewrite if -m flag present
 }
