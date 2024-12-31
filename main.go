72ee76db969de7748f5be26a0cadb067f5b122aa
Mustafa Ozturk <mozturkpro@gmail.com>
2024-12-30 19:14:50 -0500

    move pubDate extractor

--- main.go
+++ main.go
@@ -2,19 +2,9 @@ package main
 
 import (
 	"log"
-	"time"
-	"strings"
 
 	"github.com/mustafa-ozturk/rsslagg/internal/config"
 )
-
-func extractTimeFromPubDate(originalPubDate string) (time.Time, error) {
-	splitDate := strings.Split(originalPubDate, " ")
-	joinedDate := strings.Join(splitDate[:len(splitDate) - 2], " ")
-	pubDate, err := time.Parse("Mon, 02 Jan 2006", joinedDate)
-	return pubDate, err
-}
-
 
 func main() {
 	cfg, err := config.Read()
--- rss.go
+++ rss.go
@@ -9,6 +9,7 @@ import (
 	"html"
 	"sort"
 	"fmt"
+	"strings"
 )
 
 type RSSFeed struct {
@@ -119,4 +120,11 @@ func PrintRSSItems(rssItems []RSSItemWithChannelTitle, maxPostsDisplayed int) {
 			item.ItemTitle,
 			item.Link) 
 	}
+}
+
+func extractTimeFromPubDate(originalPubDate string) (time.Time, error) {
+	splitDate := strings.Split(originalPubDate, " ")
+	joinedDate := strings.Join(splitDate[:len(splitDate) - 2], " ")
+	pubDate, err := time.Parse("Mon, 02 Jan 2006", joinedDate)
+	return pubDate, err
 }
